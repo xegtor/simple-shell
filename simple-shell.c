e5642558591662e5dd1cc75777e335ad2020d534
Xegtor <rohakkansal04@gmail.com>
2023-09-06 21:48:30 +0530

    progress

--- simple-shell.c
+++ simple-shell.c
@@ -3,18 +3,19 @@
 #include <string.h>
 #include <signal.h>
 
-volitile sig_atomic_t ctrl_c = 0;
+volatile sig_atomic_t ctrl_c = 0;
 
 // Signal handler function to catch Ctrl+C
 void sigint_handler(int signum) {
     (void)signum;  // Suppress unused variable warning
     ctrl_c = 1;
+    exit(EXIT_FAILURE);  // Terminate the program immediately
 }
 
 int is_valid_cmd();
 
 int main() {
-    string input;
+    char input[1000000];
     signal(SIGINT, sigint_handler); //registering the ctrl+c signal
     while(!ctrl_c) {
         int cmd_valid = 0;
@@ -29,7 +30,26 @@ int main() {
             if (strlen(input) == 0) {
                 continue;  // Prompt for input again
             }
-            char* command = strtok()
+            char* token = strtok(input, "|");
+            if (token != NULL) {
+                // '|' exists in the input
+                printf("Pipeline detected.\n");
+                cmd_valid = 1;
+                // Now you can process the individual commands in the pipeline
+                // For example, you can further tokenize each part of the pipeline
+                while (token != NULL) {
+                    // Process the token as a command
+                    printf("Command: %s\n", token);
+
+                    // Get the next token
+                    token = strtok(NULL, "|");
+                }
+            } else {
+                // '|' does not exist in the input
+                printf("No pipeline detected.\n");
+                cmd_valid = 1;
+                // You can handle the case where '|' is not present, and this is a single command
+            }
         }
     }
     return 0;
