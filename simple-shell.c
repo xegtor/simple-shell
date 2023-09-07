c06ee8888a3e9b1642ad89f1664cc829a74ee36d
Xegtor <rohakkansal04@gmail.com>
2023-09-07 17:39:47 +0530

    progress

--- simple-shell.c
+++ simple-shell.c
@@ -9,17 +9,35 @@ volatile sig_atomic_t ctrl_c = 0;
 void sigint_handler(int signum) {
     (void)signum;  // Suppress unused variable warning
     ctrl_c = 1;
-    exit(EXIT_FAILURE);  // Terminate the program immediately
+    printf("\n");
+    exit(0);  // Terminate the program immediately
 }
 
-int is_valid_cmd();
+int is_valid_cmd(char* input){
+    char* token = strtok(input, " ");
+    if(input[0] == '.' && input[1] == '/') return 1; // returns 1 for './' cmd
+    else if(strcmp(token,"cd") == 0) return 2; //returns 2 for 'cd' cmd
+    else if(strcmp(token, "mkdir") == 0) return 3; // returns 3 for 'mkdir' cmd
+    else if(strcmp(token,"rmdir") == 0) return 4; // returns 4 for 'rmdir' cmd
+    else if(strcmp(token,"rm") == 0) return 5; // returns 5 for 'rm' cmd
+    else if(strcmp(token,"mv") == 0) return 6; // returns 6 for 'mv' cmd
+    else if(strcmp(token,"cp") == 0) return 7; // returns 7 for 'cp' cmd
+    else if(strcmp(token,"pwd") == 0) return 8; // returns 8 for 'pwd' cmd
+    else if(strcmp(token,"uniq") == 0) return 9; // returns 9 for 'uniq' cmd
+    else if(strcmp(token,"sort") == 0) return 10; // returns 10 for 'sort' cmd
+    else if(strcmp(token,"grep") == 0) return 11; // returns 11 for 'grep' cmd
+    else if(strcmp(token,"wc") == 0) return 12; // returns 12 for 'wc' cmd
+    else if(strcmp(token,"echo") == 0) return 13; // returns 13 for 'echo' cmd
+    else if(strcmp(token,"ls") == 0) return 14; // returns 14 for 'ls' cmd
+    else return 0; // invalid cmd
+}
 
 int main() {
     char input[1000000];
     signal(SIGINT, sigint_handler); //registering the ctrl+c signal
     while(!ctrl_c) {
-        int cmd_valid = 0;
-        while (!cmd_valid){
+        int cmd = 0;
+        while (!cmd){
             printf("simple-shell> ");
             if (fgets(input, sizeof(input), stdin) == NULL) {
                 printf("\n");
@@ -30,25 +48,15 @@ int main() {
             if (strlen(input) == 0) {
                 continue;  // Prompt for input again
             }
-            char* token = strtok(input, "|");
-            if (token != NULL) {
-                // '|' exists in the input
-                printf("Pipeline detected.\n");
-                cmd_valid = 1;
-                // Now you can process the individual commands in the pipeline
-                // For example, you can further tokenize each part of the pipeline
-                while (token != NULL) {
-                    // Process the token as a command
-                    printf("Command: %s\n", token);
-
-                    // Get the next token
-                    token = strtok(NULL, "|");
-                }
-            } else {
-                // '|' does not exist in the input
-                printf("No pipeline detected.\n");
-                cmd_valid = 1;
-                // You can handle the case where '|' is not present, and this is a single command
+            if(strchr(input, '|') == NULL){
+                printf("No pipelining\n");
+                printf("%s\n",input);
+                cmd = is_valid_cmd(input);
+                printf("%d\n", cmd);
+            }else{
+                printf("Pipelining detecting\n");
+                printf("%s\n",input);
+                cmd = 1;
             }
         }
     }
